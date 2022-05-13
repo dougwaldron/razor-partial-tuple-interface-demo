f6f471400897b4d6153c10e9bb62fea40a2d6f83
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-05-13 10:46:25 -0400

    Add model for partial view

--- SampleInterface.cs
+++ SampleInterface.cs
@@ -7,3 +7,11 @@ public class ImplementationClass : ISampleInterface
 {
     public int Value => 5;
 }
+
+public class ModelForPartial
+{
+    public ModelForPartial(ISampleInterface s, int n) => (Sample, Number) = (s, n);
+
+    public ISampleInterface Sample { get; }
+    public int Number { get; }
+}
